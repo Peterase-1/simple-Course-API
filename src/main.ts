262d53be27dedbf213f080bc6d0d509cb4e1f123
Petros Asegid <pasegid@gmail.com>
2026-02-20 11:45:00 +0300

    Scaffold root module and setup validation and Swagger pipes

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Enable validation globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Set up Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Course API')
+    .setDescription('The simple Course API description')
+    .setVersion('1.0')
+    .addTag('courses')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
