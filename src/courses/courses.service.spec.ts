b7d41d6c1b886098b8f4ef9091d70ca11958cfb5
Petros Asegid <pasegid@gmail.com>
2026-02-22 09:20:00 +0300

    Add unit tests for CoursesService

---
+++ src/courses/courses.service.spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CoursesService } from './courses.service';
+import { NotFoundException } from '@nestjs/common';
+
+describe('CoursesService', () => {
+  let service: CoursesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CoursesService],
+    }).compile();
+
+    service = module.get<CoursesService>(CoursesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should create a course', () => {
+    const newCourse = service.create({
+      title: 'Intro to NestJS',
+      level: 'Advanced',
+      duration: '6 weeks',
+    });
+    expect(newCourse).toHaveProperty('id');
+    expect(newCourse.title).toBe('Intro to NestJS');
+    expect(service.findAll().length).toBe(1);
+  });
+
+  it('should throw NotFoundException for invalid ID', () => {
+    expect(() => service.findOne('999')).toThrow(NotFoundException);
+  });
+});
